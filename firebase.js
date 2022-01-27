97568b590d6bb704902383e81db6c9a09e456073
ian038 <ianphua3@gmail.com>
2022-01-27 16:53:50 -0500

    Setup firebase

---
+++ firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.4/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.4/firebase-analytics.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC9wqzAGUMhy_O2XL3pHpXjgKUuMPfTAVA",
+    authDomain: "amz-clone-4eda4.firebaseapp.com",
+    projectId: "amz-clone-4eda4",
+    storageBucket: "amz-clone-4eda4.appspot.com",
+    messagingSenderId: "160803880842",
+    appId: "1:160803880842:web:db23caaf5eff7005b3d273",
+    measurementId: "G-413S1GY27H"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+let db = firebase.firestore()
